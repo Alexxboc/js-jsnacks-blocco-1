a2a1ab70ab543778fe8dbb141e5527b70d49343e
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-30 15:00:01 +0200

    Complete sanck 6

---
+++ snack_6/main.js
@@ -0,0 +1,21 @@
+/* 
+Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente.
+*/
+
+// Crea un array vuoto
+
+const arrayNumbers = []
+
+// Chiedi all'utente quanti numeri dobbiamo utilizzare
+let user_numbers = Number(prompt(`Type a number`));
+
+// Aggiungi il numero scelto all'array
+arrayNumbers.push(user_numbers)
+
+// Crea un ciclo di numeri
+for(let i = 0; i < arrayNumbers; i++){
+    console.log(Math.pow(i,3));
+}
+
+
