fc46c19de3d086900aa82a79e5e8567132826c12
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-30 15:11:41 +0200

    Complete sanck 7

---
+++ snack_7/main.js
@@ -0,0 +1,15 @@
+/*
+Stampa le potenze di 2 fino a 1000.
+*/
+
+// Creo una variante per il numero da utilizzare nell'operazione
+
+const number = 2;
+
+// Creo un ciclo di 1000 iterazioni
+for(let i = 1; i < 1000 + 1; i++){
+
+// Stampo in console le potenze di 2 fino a 1000
+    console.log(Math.pow(number,i))
+    
+}
