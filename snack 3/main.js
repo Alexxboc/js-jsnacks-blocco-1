0979a5ab1a54513486e626ea120bc66ce934c803
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-30 11:55:37 +0200

    add comments

--- snack 3/main.js
+++ snack 3/main.js
@@ -7,13 +7,14 @@ Il programma stampa la somma di tutti i numeri inseriti.
 */
 
 // Dichiaro una variabile per la somma
-
 let sum = 0
 
-
+// Crea un ciclo che si ripeta 10 volte
 for (let i = 0; i < 10; i++) {
 // Chiedere all'utente di inserire un numero per 10 volte
     let numbers = Number(prompt(`type a number`)) 
+
+//Calcola la somma dei numeri inseriti
     sum += numbers
     console.log(sum);
     
