5aa01953f7cb8c43316ec5ae66e3f6769baea859
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-30 16:24:43 +0200

    Add comment

--- snack_9/main.js
+++ snack_9/main.js
@@ -5,7 +5,7 @@ Calcola la somma e la media dei primi 10 numeri.
 // Creo un array con i primi 10 numeri
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Dichiaro una costante per la somma
+// Dichiaro una variabile per la somma
 let sum = 0;
 
 // Creo un ciclo di iterazioni
