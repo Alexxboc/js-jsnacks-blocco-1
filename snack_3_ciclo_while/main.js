f1157c8bb064dd41a727ef8b3d773b2d92cf37a3
Alexxboc <boccardi.alessandro@gmail.com>
2022-03-31 11:48:03 +0200

    add comments

--- snack_3_ciclo_while/main.js
+++ snack_3_ciclo_while/main.js
@@ -6,14 +6,24 @@ Il programma stampa la somma di tutti i numeri inseriti.
 
 */
 
+// Inizializzo una variabile per la somma
 let sum = 0;
+
+// Inizializzo una variabile per l'indice
 let i = 0;
 
+// Creo un ciclo while che dovrà ripetersi finchè l'utente non ha inserito 10 volte il numero
 while(i < 10) {
-    let numbers = Number(prompt(`type a number`));
-    sum += numbers;
 
+    // Chiedo all'utente di inserire un numero per 10 volte
+    let numbers = Number(prompt(`type a number`));
+    // Faccio la somma di tutti i numeri
+    sum += numbers;
+    // Indico il valore di incremento
     i++
 }
 
+// 
+
+// Stampo in console la somma
 console.log(sum);
